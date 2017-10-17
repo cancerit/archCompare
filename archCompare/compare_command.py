67884e384eee2c13b66c7ac0fefedafdcb745b2e
Shriram Bhosle <sb43@sanger.ac.uk>
2017-10-17 12:22:41 +0100

    working version with travis

---
+++ archCompare/compare_command.py
@@ -0,0 +1,40 @@
+import sys,os
+import argparse
+import pkg_resources 
+
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__),'..')))
+
+from  archCompare.compareArchive import ArchCompare
+version = pkg_resources.require("archCompare")[0].version
+
+def main():
+    
+    usage = "\n %prog [options] -a archive_a.tar -b archive_b.tar -j fileTypes.json -c name"
+    
+    optParser = argparse.ArgumentParser(prog='cgpCompare')
+    optParser.add_argument( "-a", "--archive_a", type=str, dest="archive_a",
+    default = "", help = "archive path for a folder, file or a tar data" )
+    
+    optParser.add_argument( "-b", "--archive_b", type=str, dest="archive_b",
+    default = "", help = "archive path for a folder, file or a tar data" )
+    
+    optParser.add_argument( "-j", "--json_config", type=str, dest="json_config",
+    default = "", help = "path to json config file" )
+    
+    optParser.add_argument( "-c", "--cmp_type", type=str, dest="cmp_type", required=False, \
+    default = None, help = "Compariosn type to perform [ \
+     name: compares archives using file names, \
+     checksum: perform comparsion based on checksum tool defined in json config file \
+     data: does full data comparison based on tools defined for each extension type \
+     Note- command line option if set overrides default value in json config file ]" )
+    
+    optParser.add_argument("-v", "--version", action='version', version='%(prog)s ' + version)
+    optParser.add_argument("-q", action="store_false", dest="verbose", default=True)
+
+    if len( sys.argv ) == 1:
+        optParser.print_help()
+        sys.exit( 1 )
+    opts=optParser.parse_args()
+    print("Running analysis, please check log file for any errors \ndetailed results in results.tsv file")
+    mycomp=ArchCompare(archive_a=opts.archive_a,archive_b=opts.archive_b,json_config=opts.json_config,cmp_type=opts.cmp_type)
+    mycomp.run_comparison()
