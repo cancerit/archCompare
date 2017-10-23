d8b5c1494810e30f57b438ddc37107bbcd3670a2
Shriram Bhosle <sb43@sanger.ac.uk>
2017-10-23 13:37:45 +0100

    reformmated code

--- archCompare/compare_command.py
+++ archCompare/compare_command.py
@@ -3,6 +3,7 @@ import sys
 import os
 import argparse
 import pkg_resources
+
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 version = pkg_resources.require("archCompare")[0].version
 
@@ -15,19 +16,19 @@ def main():
     required = optParser.add_argument_group('required arguments')
 
     required.add_argument("-a", "--archive_a", type=str, dest="archive_a", required=True,
-                           default="", help="archive path for a folder, file or a tar data")
+                          default="", help="archive path for a folder, file or a tar data")
 
     required.add_argument("-b", "--archive_b", type=str, dest="archive_b", required=True,
-                           default="", help="archive path for a folder, file or a tar data")
+                          default="", help="archive path for a folder, file or a tar data")
 
     optional.add_argument("-j", "--json_config", type=str, dest="json_config",
-                           default=None, help="path to json config file")
+                          default=None, help="path to json config file")
 
     optional.add_argument("-o", "--outfile", type=str, dest="outfile",
-                           default=None, help="path to outfile file, STOUT if not provided")
+                          default=None, help="path to outfile file, STOUT if not provided")
 
     optional.add_argument("-c", "--cmp_type", type=str, dest="cmp_type", required=False,
-                           default=None, help="Compariosn type to perform [ \
+                          default=None, help="Compariosn type to perform [ \
                            name: compares archives using file names, \
                            checksum: perform comparsion based on checksum tool defined in json config file \
                            data: does full data comparison based on tools defined for each extension type \
