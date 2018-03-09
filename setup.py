cabca1ff6895d3d3fa2297db0bcd80089f39abd9
David Jones <drj@sanger.ac.uk>
2018-03-09 13:36:04 +0000

    Bumped version to 1.1.4

--- setup.py
+++ setup.py
@@ -3,13 +3,13 @@
 from setuptools import setup
 
 config = {
-    'version': '1.1.3',
+    'version': '1.1.4',
     'name': 'archCompare',
     'description': 'tool to comapre files and/or archives',
     'author': 'Shriram G Bhosle',
     'url': 'https://github.com/CancerIT/archCompare',
     'author_email': 'cgphelp@sanger.ac.uk',
-    'python_requires': '>= 3.3',	
+    'python_requires': '>= 3.3',
     'setup_requires': ['pytest','pytest-cov'],
     'install_requires': ['beautifultable'],
     'packages': ['archCompare'],
